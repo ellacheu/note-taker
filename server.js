3f7ece86e0dfa89014384acb7db5833522bf00ae
elayne <elacheu@gmail.com>
2023-11-21 23:07:51 -0600

    routes

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const fs = require('fs')
-const path = requite('path')
+const path = require('path')
+const api = require('./routes/index.js');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
 const dbPath = path.join(__dirname, 'db', 'db.json');
 
-const readNotes = () => {
-    try {
-        const dbData = fs.readFileSync(dbPath, 'utf8');
-        return JSON.parse(dbData);
-    } catch (error) {
-        console.error('Error reading from database', error);
-        return [];
-    }
-};
 
 
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+
+);
+  
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
